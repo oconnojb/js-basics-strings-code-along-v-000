d5cc195ede18e475202a98cf918f431712270452
John O'Connor <oconnojb@gmail.com>
2018-06-04 17:51:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Write your code in this file!
 let currentUser = "John"
 let welcomeMessage = "Welcome to Flatbook, " + currentUser +"!"
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() +"!"
